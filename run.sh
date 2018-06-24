7ec095b816c38e0096d77da23e6fb2dc411370cf
Olaolu Akinsete <akinsbo@gmail.com>
2018-06-24 23:09:47 +0100

    fixed connection of bastion

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
-ansible-playbook -i hosts.ini install-kubectl.yaml --ask-become-pass
+# ansible-playbook -i hosts.ini install-kubectl.yaml --ask-become-pass
 
-ansible-playbook -i hosts.ini install-kubectl.yaml --extra_vars "controller=vagrant" --ask-become-pass
+# ansible-playbook -i hosts.ini install-kubectl.yaml --extra_vars "controller=vagrant" --ask-become-pass
+
+ansible-playbook -i inventory webservers.yaml --ask-become-pass -vvvv   
+
+# Call the run bastion playbook with ec2.py and parameters
+ansible-playbook -i inventory run_from_bastion.yaml --ask-become-pass
+
+# ssh -o IdentitiesOnly=yes-i /home/olaolu/ec2_keys/eu-west-1/mbshow_testi.pem admin@bastion.k8s.maryboye.org
